356fb94c52cad0a871dc45801a9af2cf2d9fbb3b
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2020-12-01 18:34:15 +0100

    Home

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-useless-constructor */
+import React from "react";
+import './Home.scss'
+
+const Home = (props) =>{
+
+    const titulo = "BIENVENIDOS A NUESTRA CLINICA DENTAL";
+    let foto = {photo:'./dental.jpg'}
+    
+   
+        return(
+            <div className="Home">
+                <h1 className="h1">{titulo}</h1>
+
+                <img src={foto.photo} alt="Dientes" className="foto"/>               
+            </div>
+        );
+    
+}
+
+
+
+export default Home;
