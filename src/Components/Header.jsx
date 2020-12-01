09aa7878a40a0f862191dd9e6e235c1b5aa48f7e
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2020-12-01 18:25:53 +0100

    Componente Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Citas from '../containers/Citas/Citas'
+import './Header.scss'
+const Header = (props) => {
+    const logout = ()=>{
+        localStorage.removeItem('token')
+        props.setUser(null)
+    }
+    return (
+        <header className="header">
+            <NavLink to="/" className="home1">Home</NavLink>
+            {props.user ?
+                    <>
+                        <span className="email">{props.user?.email}</span>
+                        <span onClick={logout} className="logout">Logout</span>
+                        <span onClick={Citas} className="citas">Citas</span>
+                        
+                    </> :
+                    <>
+                        <NavLink to="/login" className="login1">Log in</NavLink>
+                        <NavLink to="/register" className="register1">Register</NavLink>
+                    </>
+                    
+                }
+               
+            
+        </header>
+    )
+}
+export default Header;
