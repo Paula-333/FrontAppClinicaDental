048ed919c0383b55e314f97aee12c3fbd73353b3
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2020-12-01 18:32:40 +0100

    Login

---
+++ src/Containers/Login/Login.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, {useState } from 'react';
+import './Login.scss'
+import { useHistory } from 'react-router-dom';
+
+
+const Login = (props) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+    const handleSubmit = async (event) => {
+        try {
+            event.preventDefault();
+            const res = await axios.post('https://clinia-dental-citas.herokuapp.com/user/login', {email,password})
+            localStorage.setItem('token',res.data.token)
+            props.setUser(res.data.user)
+            history.push('/')
+        } catch (error) {
+            console.log('ERROR')
+        }
+    
+    }
+    return (
+        <div className="Login" onSubmit={handleSubmit}>
+            <h1>¡Login!</h1>
+            <input type="email" onChange={event=>setEmail(event.target.value)} name="email" placeholder="Email" value={email} className="input-1"/>
+            <input type="password" onChange={event=>setPassword(event.target.value)} name="password" placeholder="Contraseña" value={password} className="input-1"/>
+            <button type="submit" className="button">Login</button>
+        </div>
+    )
+}
+
+
+export default Login;
